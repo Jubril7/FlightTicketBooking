899ca85a526a358d54f2aeef7a219675f591dced
Jubril <jubrilbucknor@gmail.com>
2023-11-15 08:53:05 +0100

    first commit

---
+++ src/test/java/com/flightbooking/FlightBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
